548b882314db083b3eef328467e1a3c2439e215c
izumiayaa <A110082018@mail.shu.edu.tw>
2022-11-14 21:45:39 +0800

    feat. count enemys count to load scene

--- Assets/Scripts/inGameUI.cs
+++ Assets/Scripts/inGameUI.cs
@@ -1,23 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class inGameUI : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void Button_Menu()
     {
 
@@ -35,10 +25,41 @@ public class inGameUI : MonoBehaviour
                 Pet.isKeepShooting = 0;
             }
     }
+    // player shoot
     public void Button_CharacterShot()
     {
         Debug.Log("Shoot!");
         Instantiate(bulletPrefab, firePoint.transform.position, transform.rotation);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
         
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NextLevel();   
+    }
+
+    public void NextLevel()
+    {
+        GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        int enemyCount = enemys.Length;
+        Debug.Log(enemyCount);
+        if (enemyCount == 0)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 5)
+            {
+                return;
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+    
 }
